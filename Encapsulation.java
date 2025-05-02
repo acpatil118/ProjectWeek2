2955e5e30d62a696b5621911267c67f42ed29d15
acpatil118 <acp.archu@gmail.com>
2025-05-02 07:00:14 -0700

    TestCommit

---
+++ Encapsulation.java
@@ -0,0 +1,57 @@
+package BasicJavaProgram;
+
+public class Encapsulation {
+
+	
+	class SensitiveInformation
+	{
+		private String username="contact@grotechminds.com";
+		public String getUsername()
+		{
+			return username;
+		}
+		public void setUsername(String username)
+		{
+			this.username=username;
+		}
+		private int otp=1234;
+		public int getOtp()
+		{
+			return otp;
+		}
+		public void setOtp(int otp)
+		{
+			this.otp=otp;
+		}
+		private String passowrd="jhtr56";
+		public String getPassowrd() {
+			return passowrd;
+		}
+		public void setPassowrd(String passowrd) {
+			this.passowrd = passowrd;
+		}
+		
+		
+	}
+	public class EncapsulationProgram 
+	{
+	
+	{
+		SensitiveInformation s1=new SensitiveInformation();
+		s1.setUsername("manish@grotechminds.com");
+		System.out.println(s1.getUsername());
+		s1.setOtp(5454);
+		System.out.println(s1.getOtp());
+		
+				}
+	}
+	
+	
+	
+
+}
+
+
+
+
+
